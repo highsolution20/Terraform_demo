8f52664dc8abdb39d15d5bf404764031492e1703
highsolution20 <113486740+highsolution20@users.noreply.github.com>
2023-03-29 16:43:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,12 @@
 provider "aws" {
-  region = "us-west-2"
+  region     = "ap-south-1"
 }
-resource "aws_instance" "tfvm" {
-  ami = "ami-0735c191cf914754d"
+
+resource "aws_instance" "Demo" {
+  ami           = "ami-0d758c1134823146a"
   instance_type = "t2.micro"
-  key_name = "newServerKey"
-  vpc_security_group_ids = [ aws_security_group.websg.id ]
-  user_data = <<-EOF
-                #!/bin/bash
-                echo "High Solution Terraform " > index.html
-                nohup busybox httpd -f -p 8080 &
-                EOF
-    tags = {
-      Name = "WEB-TerraDemo"
-    }
-}
-resource "aws_security_group" "websg" {
-  name = "web-sg01"
-  ingress {
-    protocol = "tcp"
-    from_port = 8080
-    to_port = 8080
-    cidr_blocks = [ "0.0.0.0/0" ]
+  key_name = "OreoKey"
+  tags = {
+    Name = "Demo"
   }
-}
-output "instance_ips" {
-  value = aws_instance.tfvm.public_ip
 }
