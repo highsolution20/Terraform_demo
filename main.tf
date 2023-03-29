074f977f366777c224ee8aff85255fed3733f68e
highsolution20 <113486740+highsolution20@users.noreply.github.com>
2023-03-29 18:56:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  shared_credentials_file = "/Users/briti/.aws/credentials"
-  profile                 = "default"
+  access_key = "AKIAS4RY27TJ3N4MRIBL"
+  secret_key = "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"
   region     = "ap-south-1"
 }
 
