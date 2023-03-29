58bc72e60286986465e17b237e302213035a2898
highsolution20 <113486740+highsolution20@users.noreply.github.com>
2023-03-29 17:31:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAS4RY27TJ5QHM4DWS"
-  secret_key = "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"
 }
 
 resource "aws_instance" "Demo" {
