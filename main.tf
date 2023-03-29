2e2d270949c6fc48ec4b23f6df09621db81b0949
highsolution20 <113486740+highsolution20@users.noreply.github.com>
2023-03-28 21:48:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,31 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
   region = "us-west-2"
+  access_key = "AKIAS4RY27TJ5QHM4DWS"
+  secret_key = "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"
 }
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"             # "ami-0a634ae95e11c6f91"
+resource "aws_instance" "tfvm" {
+  ami = "ami-0735c191cf914754d"
   instance_type = "t2.micro"
-  associate_public_ip_address = true
-  key_name                    = "newServerKey"
-  vpc_security_group_ids      = ["sg-0362a69872d0dc3f7"]
-
-  tags = {
-    Name = "highsolutionTerraform"
+  key_name = "newServerKey"
+  vpc_security_group_ids = [ aws_security_group.websg.id ]
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "High Solution Terraform " > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+    tags = {
+      Name = "WEB-Terra"
+    }
+}
+resource "aws_security_group" "websg" {
+  name = "web-sg01"
+  ingress {
+    protocol = "tcp"
+    from_port = 8080
+    to_port = 8080
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
+}
+output "instance_ips" {
+  value = aws_instance.tfvm.public_ip
 }
