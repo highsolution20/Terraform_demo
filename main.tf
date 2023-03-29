70fc7be57e767ce3db78a4e370b04e0e396fd192
highsolution20 <113486740+highsolution20@users.noreply.github.com>
2023-03-28 21:10:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
-###
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"             # "ami-0a634ae95e11c6f91"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = "newServerKey"
+  vpc_security_group_ids      = ["sg-0362a69872d0dc3f7"]
+
+  tags = {
+    Name = "highsolutionTerraform"
+  }
+}
